ee977bdadf084d0b7b48600cbf98111201443489
Yoshito Mitsui <pooh.aflac@gmail.com>
2017-12-21 01:35:31 +0900

    init commit

---
+++ PolyWorld/Assets/Grobe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Grobe : MonoBehaviour {
+
+    public GameObject ball;
+    private bool holdingBall = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (holdingBall)
+        {
+            ball.transform.position = gameObject.transform.position + gameObject.transform.forward;
+
+            if (Input.GetKeyDown(KeyCode.JoystickButton5))
+            {
+                holdingBall = false;
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+        holdingBall = true;
+    }
+}
---
+++ PolyWorld/Assets/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+using UnityEngine.XR.WSA.Input;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject ball;
+    public GameObject playerCamera;
+
+    public float ballDistance = 2f;
+    public float ballThrowingForce = 5f;
+
+    private bool holdingBall = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        ball.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DownKeyCheck();
+        if (holdingBall)
+        {
+            ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;
+
+            if (Input.GetKeyUp(KeyCode.JoystickButton5))
+            {
+                holdingBall = false;
+                ball.GetComponent<Rigidbody>().useGravity = true;
+                ball.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * ballThrowingForce);
+            }
+        } else
+        {
+            if (Input.GetKeyDown(KeyCode.JoystickButton5))
+            {
+                holdingBall = true;
+            }
+        }
+
+
+    }
+
+    void DownKeyCheck()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(code))
+                {
+                    //処理を書く
+                    Debug.Log(code);
+                    break;
+                }
+            }
+        }
+    }
+}
